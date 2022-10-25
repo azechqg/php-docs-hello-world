ba75aae612765aecf9c3eebda78abd2e449bae32
azechqg <116661923+azechqg@users.noreply.github.com>
2022-10-25 11:38:17 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
 <?php
 
-echo "Hello World! ";
-    function getIPAddress() {  
-    //whether ip is from the share internet  
-     if(!emptyempty($_SERVER['HTTP_CLIENT_IP'])) {  
-                $ip = $_SERVER['HTTP_CLIENT_IP'];  
-        }  
-    //whether ip is from the proxy  
-    elseif (!emptyempty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
-                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
-     }  
-//whether ip is from the remote address  
-    else{  
-             $ip = $_SERVER['REMOTE_ADDR'];  
-     }  
-     return $ip;  
-}  
-$ip = getIPAddress();  
-echo "User Real IP Address - ".$ip;  
+echo "Hello World!"; 
+echo $_SERVER['HTTP_CLIENT_IP']; 
+echo $_SERVER['HTTP_X_FORWARDED_FOR']; 
+echo $_SERVER['REMOTE_ADDR'];
 ?> 
